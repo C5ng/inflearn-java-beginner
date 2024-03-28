d3f75118fb415748eba081696e484cb6250a841e
공대표 <c5ngsdp@gmail.com>
2024-03-28 17:01:14 +0900

    Section 2. 변수
    변수 선언과 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a); // 주석을 풀고 실행해도 컴파일 x -> out directory에 생성 x
+    }
+}

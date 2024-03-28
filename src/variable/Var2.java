c2dc734e8926515096605bc20538ef31d329898e
공대표 <c5ngsdp@gmail.com>
2024-03-28 16:36:52 +0900

    Section 2. 변수
    변수 시작

---
+++ src/variable/Var2.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 20; // 변수 초기화
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+        // 만약 0을 출력하는 것이 아닌 20을 출력 ?
+        // 변수의 저장한 값을 20으로 변경 a = 10 -> a = 20
+
+        String str1 = "노수현";
+        String str2 = "노수현";
+
+        if (str1 == str2) {
+        }
+    }
+}

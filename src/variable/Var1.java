79c73f589edc30645acc30b49d05997dbb8b0e22
공대표 <c5ngsdp@gmail.com>
2024-05-20 21:07:16 +0900

    Section 2. 변수
    변수 시작

---
+++ src/variable/Var1.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+
+        // 요구사항: 10을 20으로 변환.
+
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
+    }
+}
---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 10; // 변수 초기화 Var1에서와 달리 a 값만 변경하면 손쉽게 여러번 출력 가능.
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}

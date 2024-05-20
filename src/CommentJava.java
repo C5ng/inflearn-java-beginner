8068cd903fedbc97d5541ad3a1b6d59a45fb51b8
공대표 <c5ngsdp@gmail.com>
2024-05-20 20:43:38 +0900

    Section 1. Hello World
    주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1 출력. -> 한 줄 주석: // 뒤에 해당 라인을 전체 무시한다.
+        System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        여러 줄 주석: 해당 사이 모든 코드 무시한다.
+         */
+    }
+}

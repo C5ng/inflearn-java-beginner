6a9e320bca1affd5e19c8c279c977c9b5837559a
공대표 <c5ngsdp@gmail.com>
2024-05-20 21:24:46 +0900

    Section 2. 변수
    변수 선언과 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a; // 변수를 선언하고 사용하지 않으면 컴파일 단계에서 최적화된다.
+//        System.out.println(a); // 변수를 초기화하지 않고 사용하게 되면 아예 오류가 난다.
+    }
+}

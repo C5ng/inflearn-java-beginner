ef8dacdfcaa4725a423eb56232e0519420f782ad
공대표 <c5ngsdp@gmail.com>
2024-05-21 22:39:01 +0900

    Section 3. 연산자
    대입 연산자

---
+++ src/operator/Assign1.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Assign1 {
+    public static void main(String[] args) {
+        // 축약(복합) 대입 연산자 +=, *= ...
+        int a = 5;
+        a += 3; // 8
+        a -= 2; // 6
+        a *= 4; // 24
+        System.out.println("a = " + a);
+    }
+}

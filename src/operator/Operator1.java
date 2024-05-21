b78cf9e5ea41a2d35c3f77c76bcdb1b286ea0090
공대표 <c5ngsdp@gmail.com>
2024-05-21 22:09:47 +0900

    Section 3. 연산자
    산술 연산자

---
+++ src/operator/Operator1.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int sum = a + b; // 덧셈
+        System.out.println(sum);
+
+        int diff = a - b; // 뺄셈
+        System.out.println(diff);
+
+        int multi = a * b; // 덧셈
+        System.out.println(multi);
+
+        int div = a / b;
+        System.out.println(div); // 나눗셈
+
+        int mod = a % b;
+        System.out.println(mod); // 나머지
+    }
+}

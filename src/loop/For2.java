47feb9cbc5128b8ea23901f0cd99c80bbaf87b0b
공대표 <c5ngsdp@gmail.com>
2024-05-22 21:43:36 +0900

    Section 5. 반복문
    for문 1

---
+++ src/loop/For2.java
@@ -0,0 +1,10 @@
+package loop;
+
+public class For2 {
+    public static void main(String[] args) {
+        int endNum = 3;
+        for (int i = 1; i <= endNum; i++) {
+            System.out.println(i);
+        }
+    }
+}

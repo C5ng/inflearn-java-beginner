62057320776a71b12fe496eb0bb1be0e252e630a
공대표 <c5ngsdp@gmail.com>
2024-05-23 08:24:15 +0900

    Section 7. 훈련
    Scanner 학습

---
+++ src/scanner/Scanner1.java
@@ -0,0 +1,21 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자열을 입력하세요: ");
+        String str = scanner.nextLine(); // 입력을 String으로 받는다.
+        System.out.println("str = " + str);
+
+        System.out.print("정수를 입력하세요: ");
+        int intValue = scanner.nextInt();
+        System.out.println("intValue = " + intValue);
+
+        System.out.print("실수를 입력하세요: ");
+        double doubleValue = scanner.nextDouble();
+        System.out.println("doubleValue = " + doubleValue);
+    }
+}

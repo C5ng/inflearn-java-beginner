a66075e4d7b353244c76ad396c4ca9465a22df8c
공대표 <c5ngsdp@gmail.com>
2024-05-27 21:00:53 +0900

    Section 8. 배열
    2차원 배열 - 리팩토링 2

---
+++ src/array/ArrayDi4.java
@@ -0,0 +1,22 @@
+package array;
+
+public class ArrayDi4 {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][4];
+        int count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count;
+                count++;
+            }
+        }
+
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

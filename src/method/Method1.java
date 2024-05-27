68f2c417e7222171f8334a15dc4cc8d904aa2fdd
공대표 <c5ngsdp@gmail.com>
2024-05-27 21:29:49 +0900

    Section 9. 메서드
    메서드 시작

---
+++ src/method/Method1.java
@@ -0,0 +1,17 @@
+package method;
+
+public class Method1 {
+    public static void main(String[] args) {
+        // 계산 1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + " + " + b + " 수행");
+        System.out.println(a + b);
+
+        // 계산 2
+        int x = 10;
+        int y = 10;
+        System.out.println(x + " + " + y + " 수행");
+        System.out.println(x + y);
+    }
+}
